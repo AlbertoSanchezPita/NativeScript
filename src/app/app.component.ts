0f2d39d258f25c806afaa922c33c22c5c3b5e6a8
AlbertoSanchezPita <94797450+AlbertoSanchezPita@users.noreply.github.com>
2023-03-14 17:46:34 +0100

    -

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,27 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
+import { EventData } from '@nativescript/core';
+import Theme from '@nativescript/theme'
 
 @Component({
   selector: 'ns-app',
   templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
- 
+
+export class AppComponent implements OnInit{
+
+  isDark:boolean = true;
+  ngOnInit():void {
+    Theme.setMode(Theme.Dark)
+  }
+
+  changeMode(args:EventData){
+    if(this.isDark){
+      this.isDark = false;
+      Theme.setMode(Theme.Light);
+    } else {
+      this.isDark = true;
+      Theme.setMode(Theme.Dark);
+    }
+  }
 }
